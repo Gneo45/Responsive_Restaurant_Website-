50ee54f42cc9b320cdbdd8c0662850a388e1af0a
Grégory Néault <85392282+Gneo45@users.noreply.github.com>
2022-02-07 21:32:22 +0100

    initial commit

---
+++ restaurant_website/src/container/Menu/SpecialMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { SubHeading, MenuItem  } from '../../components';
+import { images, data } from '../../constants'
+
+import './SpecialMenu.css';
+
+const SpecialMenu = () => (
+  <div>
+    SpecialMenu
+  </div>
+);
+
+export default SpecialMenu;
